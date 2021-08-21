927bc0ad58c43f182363fc3b1a676684374cdcc1
Ocholla-T <tonnyocholla8@gmail.com>
2021-08-21 20:23:24 +0300

    added hero section to page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <NavigationBar />
+    <HeroSection />
   </div>
 </template>
 
 <script>
 import NavigationBar from '@/components/NavigationBar';
+import HeroSection from '@/components/HeroSection';
 
 export default {
-  components: { NavigationBar },
+  components: { NavigationBar, HeroSection },
   setup() {
     return {};
   },
