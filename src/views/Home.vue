97d8a8dd9c5ccde609fa15bce9ce988942f0e6d2
Ocholla-T <tonnyocholla8@gmail.com>
2021-08-22 18:48:07 +0300

    added call to action and services components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,19 @@
   <div>
     <NavigationBar />
     <HeroSection />
+    <CallToAction />
+    <Services />
   </div>
 </template>
 
 <script>
 import NavigationBar from '@/components/NavigationBar';
 import HeroSection from '@/components/HeroSection';
+import CallToAction from '@/components/CallToAction';
+import Services from '@/components/Services';
 
 export default {
-  components: { NavigationBar, HeroSection },
+  components: { NavigationBar, HeroSection, CallToAction, Services },
   setup() {
     return {};
   },
