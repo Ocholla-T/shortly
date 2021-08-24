fb28c5a1d7d702bbe97dc5c94ca5b6e3ce4c7639
Ocholla-T <tonnyocholla8@gmail.com>
2021-08-24 15:37:40 +0300

    init

---
+++ src/components/Services.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="services">
+    <div class="services__introduction flex flex-jc-c flex-ai-c">
+      <h2>Advanced Statistics</h2>
+      <p>
+        Track how your lists are performing across the web with our advanced statistics dashboard
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.services {
+  margin: 0 1rem;
+  &__introduction {
+    flex-direction: column;
+    text-align: center;
+    margin-bottom: 3rem;
+    > h2 {
+      color: $very-dark-violet;
+      font-size: 1.625rem;
+      margin-bottom: 1rem;
+    }
+
+    > p {
+      color: $grayish-violet;
+      font-size: 0.875rem;
+    }
+  }
+}
+</style>
