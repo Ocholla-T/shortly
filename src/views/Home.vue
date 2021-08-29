3f9166b670d08fa9157f1606b6f33e78728949d6
Ocholla-T <tonnyocholla8@gmail.com>
2021-08-29 10:38:39 +0300

    added footerCTA component to app flow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <HeroSection />
     <CallToAction />
     <Services />
+    <FooterCTA />
   </div>
 </template>
 
@@ -12,9 +13,10 @@ import NavigationBar from '@/components/NavigationBar';
 import HeroSection from '@/components/HeroSection';
 import CallToAction from '@/components/CallToAction';
 import Services from '@/components/Services';
+import FooterCTA from '@/components/FooterCTA';
 
 export default {
-  components: { NavigationBar, HeroSection, CallToAction, Services },
+  components: { NavigationBar, HeroSection, CallToAction, Services, FooterCTA },
   setup() {
     return {};
   },
